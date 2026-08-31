5c16f632d4122a5d40b9839a22ae08cc7c8c14ed
DimaManko <mankonat@gmail.com>
2026-08-31 21:59:52 +0300

    feat: integrate VideoModal into HeroSection

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -2,8 +2,29 @@ import { SideNav } from "../layout/SideNav";
 import { HeroHeader } from "./HeroHeader";
 import { HeroContent } from "./HeroContent";
 import { HeroSlider } from "./HeroSlider";
+import Loader from "../ui/Loader";
+import ErrorMessage from "../ui/ErrorMessage";
+
+import { useGetDataHeroSectionQuery } from "../../store/services/api";
+import { useState } from "react";
 
 export function HeroSection() {
+  const [isOpenVideo, setIsOpenVideo] = useState(false);
+
+  const {
+    data: { content, exploreControl } = {},
+    isLoading,
+    isError,
+  } = useGetDataHeroSectionQuery();
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return <ErrorMessage />;
+  }
+
   return (
     <section className="relative flex h-screen w-full flex-col overflow-hidden bg-white font-sans text-black">
       {/* Левый сайдбар (SideNav) */}
@@ -17,7 +38,7 @@ export function HeroSection() {
         {/* Контентная область */}
         <div className="relative flex flex-1 flex-col overflow-hidden">
           {/* Сетка Hero */}
-          <HeroContent />
+          <HeroContent {...content} />
 
           {/* Блок со слайдами (Slider Section) - min-height 307px, 104px margin right */}
           <HeroSlider />
