c862023623da55488beaa5d582c709e2f804b589
DimaManko <mankonat@gmail.com>
2026-09-01 22:06:45 +0300

    refactor(store): extract base API URL into variable

--- src/store/services/api.js
+++ src/store/services/api.js
@@ -1,13 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const _baseUrl = "http://localhost:3000";
+
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: _baseUrl }),
   endpoints: (builder) => ({
     getModules: builder.query({
       query: () => "/modules",
     }),
+    getDataHeroSection: builder.query({
+      query: () => "/heroSection",
+    }),
   }),
 });
 
-export const { useGetModulesQuery } = api;
+export const { useGetModulesQuery, useGetDataHeroSectionQuery } = api;
