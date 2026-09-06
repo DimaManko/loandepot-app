960b59989323d5429d47179f610f6ff17e0892ae
DimaManko <mankonat@gmail.com>
2026-09-06 21:18:08 +0300

    refactor(modules): refactoring modules structure on db.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-import { HeroSection } from './components/home/HeroSection.jsx'
+import { HeroSection } from "./components/home/HeroSection.jsx";
+import ModuleSection from "./components/module/ModuleSection.jsx";
 
 export default function App() {
-  return <HeroSection />
+  return (
+    <>
+      {/* <HeroSection /> */}
+      <ModuleSection />
+    </>
+  );
 }
